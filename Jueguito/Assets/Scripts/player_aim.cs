e4b601749256aaff95f86ee4b5ed57eee65712e3
lightyear9501 <71224474+lightyear9501@users.noreply.github.com>
2023-07-21 20:34:11 -0600

    Animaciones y imagenes
    Animaciones y imagenes del juego

--- Jueguito/Assets/Scripts/player_aim.cs
+++ Jueguito/Assets/Scripts/player_aim.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class player_aim : MonoBehaviour
 {
     [SerializeField] VariableJoystick joystick;
+    [SerializeField] Transform cannon; // Este es el objeto que apuntará al disparar.
+
     private void Start()
     {
         joystick.gameObject.SetActive(true);
@@ -14,6 +16,7 @@ public class player_aim : MonoBehaviour
         if (joystick.Direction == Vector2.zero) return;
         float angle = Mathf.Atan2(joystick.Vertical, joystick.Horizontal);
         float angleR = (180 / Mathf.PI) * angle - 90;
-        transform.rotation = Quaternion.Euler(0, 0, angleR);
+        cannon.rotation = Quaternion.Euler(0, 0, angleR); // Aplicar la rotación al cañón en lugar de toda la entidad del jugador.
+
     }
 }
